84910210d070d48e9e2fb20d01525004cadd0f99
devstudent89 <45929519+devstudent89@users.noreply.github.com>
2019-04-11 21:16:55 -0500

    Add robofriends build file

---
+++ build/precache-manifest.ce9e908589a5ceeab2bc399393fc9788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3b7a68a5777751e4dd48617e2d4b0ad",
+    "url": "/static/media/SEGA.e3b7a68a.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74163ebd03f833195409",
+    "url": "/static/js/main.22ebb177.chunk.js"
+  },
+  {
+    "revision": "15c7c597cae28ab6123a",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "74163ebd03f833195409",
+    "url": "/static/css/main.a568d073.chunk.css"
+  },
+  {
+    "revision": "15c7c597cae28ab6123a",
+    "url": "/static/css/2.b777cded.chunk.css"
+  },
+  {
+    "revision": "9bed0dd45033c57ab96433470f3b215d",
+    "url": "/index.html"
+  }
+];
